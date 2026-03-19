a706da6cd1980e0a04020adf4e42b71e71d68acc
Kushala <kushalasdoddangadi@gmail.com>
2026-03-19 23:30:53 +0530

    TRYING13

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://gsrqikfeirbnmoyjioyb.supabase.co'
-const supabaseKey = 'gsrqikfeirbnmoyjioyb'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzcnFpa2ZlaXJibm1veWppb3liIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyNjM3MjEsImV4cCI6MjA4MTgzOTcyMX0.EbSlHndYzhg8hVJE-pKe4optgktcJQ8wGqn1GkmqD4o'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
