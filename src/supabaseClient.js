9d1327020459dcc97434c13c9b4aedb5e1cee4e5
Kushala <kushalasdoddangadi@gmail.com>
2026-03-19 23:22:54 +0530

    TRYING11

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co'
+const supabaseUrl = 'https://https://gsrqikfeirbnmoyjioyb.supabase.co.supabase.co'
 const supabaseKey = 'gsrqikfeirbnmoyjioyb'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
