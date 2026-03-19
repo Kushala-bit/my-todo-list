4f28466e17399340ac6765bbc614f461a7758f73
Kushala <kushalasdoddangadi@gmail.com>
2026-03-19 22:20:57 +0530

    fixed by c

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co'
-const supabaseKey = 'YOUR_ANON_KEY'
+const supabaseKey = 'gsrqikfeirbnmoyjioyb'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
